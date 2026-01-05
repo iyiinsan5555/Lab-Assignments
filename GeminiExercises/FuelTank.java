954f6698845344d4aaa4242cbdfca70c5bcd4e17
iyiinsan5555 <mhbsonmez@gmail.com>
2026-01-05 21:12:42 +0300

    FuelTank Class --> Studying

---
+++ GeminiExercises/FuelTank.java
@@ -0,0 +1,32 @@
+public class FuelTank {
+    private double capacity;
+    private double currentLevel;
+
+    public FuelTank(double capacity) {
+        currentLevel = 0;
+        this.capacity = capacity;
+    }
+
+    public void addFuel(double amount) {
+        if (currentLevel + amount <= capacity) {
+            currentLevel += amount;
+        } else {
+            currentLevel = capacity;
+            System.out.println("Cannot exceed the capacity!");
+        }
+    }
+
+    public void consumeFuel(double amount) {
+        if (currentLevel - amount > 0) {
+            currentLevel -= amount;
+        } else {
+            currentLevel = 0;
+            System.out.println("Tank is empty.");
+        }
+    }
+
+    public double getFillPercentage() {
+        return currentLevel / capacity;
+    }
+
+}
